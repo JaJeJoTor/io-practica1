9d12dccfca8e6b540a73d734b62e6e4bbbf9808e
Jordi Hamberg <150976769+jordihamberg@users.noreply.github.com>
2025-10-31 23:52:19 +0100

    Fast Gradient Sign Method Attack

---
+++ FGSM.py
@@ -0,0 +1,63 @@
+import torch
+
+from torch import nn
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+from NN import NN
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def fgsm_attack(model: NN, criterion, images, labels, epsilon=0.05):
+    # Establecemos que a la hora de hacer loss.backward() también calcule 
+    # el gradiente del input (la imagen) con la propiedad requires_grad = True.
+    images.requires_grad = True
+    # Predecimos y calculamos el gradiente.
+    labels_pred = model.forward_sin_softamx(images)
+    loss = criterion(labels_pred, labels)
+    model.zero_grad()
+    loss.backward()
+    # Modificamos la imagen con el método de FGSM y lo normalizamos entre -1 y 1.
+    modification = epsilon * images.grad.data.sign()
+    modified_images= images + modification
+    modified_images = torch.clamp(modified_images, -1, 1)
+    return modified_images
+    
+if __name__ == "__main__":
+    # Cargamos el modelo.
+    model = NN(28*28)
+    model.to(device)
+    model.load_state_dict(torch.load("modelo.pth",map_location=device, weights_only=True))
+    model.eval()
+
+    # Cargamos el dataset.
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+    test_dataset  = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+    test_dataset = DataLoader(test_dataset, batch_size= 1024, pin_memory= True)
+    
+    # Calculamos los distintos accuracies para distintas epsilons.
+    
+# TODO: Gráficas y mirar a partir de qué épsilon se nota.
+    
+    for epsilon in [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3]:
+
+        correct = 0
+        total = 0
+        criterion = nn.CrossEntropyLoss(reduction='mean')
+
+        # Hacemos ataque.
+        for images, labels in test_dataset:
+            images, labels = images.to(device), labels.to(device)
+            modified_images = fgsm_attack(model, criterion, images, labels, epsilon=epsilon)
+            with torch.no_grad():
+                outputs = model(modified_images)
+                y_pred = torch.argmax(outputs, dim= 1)
+                total += labels.size(0)
+                correct += (y_pred == labels).sum().item()
+        
+        # Calculamos la accuracy.
+        accuracy = correct / total
+        if epsilon == 0.0:
+                print(f'Accuracy original: {accuracy}')
+        else:
+            print(f'Accuracy {epsilon}: {accuracy}')
